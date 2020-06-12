3ff94405bc3d90d45a6abe995a9cf2642aa80b4d
Gino Canessa <ginocanessa@gmail.com>
2020-06-12 11:48:58 -0500

    Updates for R5 May2020 Build.

--- devdays-2019-subscription-cs/Controllers/NotificationController.cs
+++ devdays-2019-subscription-cs/Controllers/NotificationController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// <copyright file="NotificationController.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+//     Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// </copyright>
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -8,168 +12,98 @@ using Newtonsoft.Json;
 
 namespace devdays_2019_subscription_cs.Controllers
 {
-    ///-------------------------------------------------------------------------------------------------
-    /// <summary>A controller for handling subcription notifications
-    /// Responds to:
-    ///     POST:   /notification
+    /// <summary>
+    /// A controller for handling Subscription notifications Responds to: POST:   /notification.
     /// </summary>
-    ///
-    /// <remarks>Gino Canessa, 11/18/2019.</remarks>
-    ///-------------------------------------------------------------------------------------------------
     [Produces("application/json")]
 
     public class NotificationController : Controller
     {
-        #region Class Variables . . .
-
         private static int _notificationCount;
 
-        #endregion Class Variables . . .
-
-        #region Instance Variables . . .
-
-        /// <summary>   The configuration. </summary>
+        /// <summary>The configuration. </summary>
         private readonly IConfiguration _config;
 
-        #endregion Instance Variables . . .
-
-        #region Constructors . . .
-
-        ///-------------------------------------------------------------------------------------------------
         /// <summary>Static constructor.</summary>
-        ///
-        /// <remarks>Gino Canessa, 11/18/2019.</remarks>
-        ///-------------------------------------------------------------------------------------------------
-
         static NotificationController()
         {
             _notificationCount = 0;
         }
 
-        ///-------------------------------------------------------------------------------------------------
         /// <summary>Constructor.</summary>
-        ///
-        /// <remarks>Gino Canessa, 11/18/2019.</remarks>
-        ///
-        /// <param name="iConfiguration">Reference to the injected configuration object</param>
-        ///-------------------------------------------------------------------------------------------------
-
-        public NotificationController(
-                                        IConfiguration iConfiguration
-                                        )
+        /// <param name="iConfiguration">Reference to the injected configuration object.</param>
+        public NotificationController(IConfiguration iConfiguration)
         {
-            // **** grab a reference to our application configuration ****
-
+            // grab a reference to our application configuration
             _config = iConfiguration;
         }
 
-        #endregion Constructors . . .
-
-        #region Class Interface . . .
-
-        #endregion Class Interface . . .
-
-        #region Instance Interface . . .
-        
-        ///-------------------------------------------------------------------------------------------------
         /// <summary>(An Action that handles HTTP POST requests) posts to a notification endpoint</summary>
-        ///
-        /// <remarks>Gino Canessa, 11/18/2019.</remarks>
-        ///
         /// <returns>An IActionResult.</returns>
-        ///-------------------------------------------------------------------------------------------------
 
         [HttpPost]
         [Route("/notification")]
         [Consumes("application/fhir+json", new[] { "application/json" })]
         public virtual IActionResult PostEventToEndpointByUid()
         {
-            // **** notify user ****
-
+            // notify user
             Console.WriteLine($"Received notification:");
 
             try
             {
-                // **** read our content ****
-
+                // read our content
                 using (StreamReader reader = new StreamReader(Request.Body))
                 {
                     string content = reader.ReadToEnd();
 
-                    // **** parse the content into a bundle ****
+                    // parse the content into a bundle
+                    fhir.Bundle bundle = JsonConvert.DeserializeObject<fhir.Bundle>(content, new fhir.ResourceConverter());
 
-                    fhir.Bundle bundle = JsonConvert.DeserializeObject<fhir.Bundle>(content);
-
-                    // **** grab the fields we need out of the notification ****
-
-                    long eventCount = -1;
-                    int bundleEventCount = -1;
-                    string status = "";
-                    string topicUrl = "";
-                    string subscriptionUrl = "";
-
-
-                    if ((bundle != null) &&
-                        (bundle.Meta != null) &&
-                        (bundle.Meta.Extension != null))
+                    if (bundle.Type != fhir.BundleTypeCodes.SUBSCRIPTION_NOTIFICATION)
                     {
-                        // **** loop over extensions ****
-
-                        foreach (fhir.Extension element in bundle.Meta.Extension)
-                        {
-                            if (element.Url.EndsWith("subscription-event-count"))
-                            {
-                                if (!Int64.TryParse(element.ValueInteger64, out eventCount))
-                                {
-                                    eventCount = -1;
-                                }
-                            }
-                            else if (element.Url.EndsWith("bundle-event-count"))
-                            {
-                                bundleEventCount = (int)element.ValueUnsignedInt;
-                            }
-                            else if (element.Url.EndsWith("subscription-status"))
-                            {
-                                status = element.ValueString;
-                            }
-                            else if (element.Url.EndsWith("subscription-topic-url"))
-                            {
-                                topicUrl = element.ValueUrl;
-                            }
-                            else if (element.Url.EndsWith("subscription-url"))
-                            {
-                                subscriptionUrl = element.ValueUrl;
-                            }
-                        }
+                        Console.WriteLine($"Invalid notification bundle.type: {bundle.Type}");
+                        return StatusCode(500);
                     }
 
-                    // **** check for being a handshake ****
+                    if ((bundle.Entry == null) || (bundle.Entry.Length == 0))
+                    {
+                        Console.WriteLine("Invalid notification MUST contain bundle.entry");
+                        return StatusCode(500);
+                    }
 
-                    if (eventCount == 0)
+                    if (bundle.Entry[0].Resource == null)
+                    {
+                        Console.WriteLine("Invalid notification, bundle.entry[0].resource MUST be a SubscriptionStatus.");
+                        return StatusCode(500);
+                    }
+
+                    fhir.SubscriptionStatus subscriptionStatus = (fhir.SubscriptionStatus)bundle.Entry[0].Resource;
+
+                    // check for being a handshake
+                    if (subscriptionStatus.NotificationType == fhir.SubscriptionStatusNotificationTypeCodes.HANDSHAKE)
                     {
                         Console.WriteLine($"Handshake:\n" +
-                            $"\tTopic:         {topicUrl}\n" +
-                            $"\tSubscription:  {subscriptionUrl}\n" +
-                            $"\tStatus:        {status}"
+                            $"\tSubscription:      {subscriptionStatus.Subscription.reference}\n" +
+                            $"\tSubscriptionTopic: {subscriptionStatus.Topic.reference}\n" +
+                            $"\tStatus:            {subscriptionStatus.Status}"
                             );
                     }
                     else
                     {
-                        Console.WriteLine($"Notification {eventCount}:\n" +
-                            $"\tTopic:         {topicUrl}\n" +
-                            $"\tSubscription:  {subscriptionUrl}\n" +
-                            $"\tStatus:        {status}\n" +
-                            $"\tBundle Events: {bundleEventCount}\n" +
-                            $"\tTotal Events:  {eventCount}"
+                        Console.WriteLine($"Notification {subscriptionStatus.EventsSinceSubscriptionStart}:\n" +
+                            $"\tSubscription:      {subscriptionStatus.Subscription.reference}\n" +
+                            $"\tSubscriptionTopic: {subscriptionStatus.Topic.reference}\n" +
+                            $"\tType:              {subscriptionStatus.NotificationType}\n" +
+                            $"\tStatus:            {subscriptionStatus.Status}\n" +
+                            $"\tTotal Events:      {subscriptionStatus.EventsSinceSubscriptionStart}\n" +
+                            $"\tBundle Events:     {subscriptionStatus.EventsInNotification}"
                             );
                     }
 
-                    // **** increment our received notification count ****
-
+                    // increment our received notification count
                     _notificationCount++;
 
-                    // **** check for being done ****
-
+                    // check for being done
                     if (_notificationCount == 2)
                     {
                         Program.DeleteSubscription().Wait(2000);
@@ -187,12 +121,5 @@ namespace devdays_2019_subscription_cs.Controllers
 
             return StatusCode(204);
         }
-        
-        #endregion Instance Interface . . .
-
-        #region Internal Functions . . .
-
-        #endregion Internal Functions . . .
-
     }
 }
